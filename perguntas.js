f2d0026f98f3c05b3dba887498e63928eb2961b6
allisonm2023 <144126531+allisonm2023@users.noreply.github.com>
2024-11-14 10:27:06 -0300

    PROJETO FLASHCARD TERMINADO

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'Conhecendo o Allison',
+    'Qual o prato preferido do Allison?',
+    'Lasanha.'
+)
+
+criaCartao(
+    'Conhecendo o Allison',
+    'Qual a cor preferida do Allison?',
+    'Azul claro.'
+)
+
+criaCartao(
+    'Conhecendo o Allison',
+    'Oque o Allison mais gosta de fazer?',
+    'jogar free fire.'
+)
+
+criaCartao(
+    'Conhecendo o Allison',
+    'Qual o esporte que o Allison gosta?',
+    'Volei e queimada.'
+)
+criaCartao(
+    'Conhecendo o Allison',
+    'Qual a pessoa que te inspira?',
+    'Minha mãe e minha avó.'
+)
+criaCartao(
+    'Conhecendo o Allison',
+    'Quais as redes sociais que o Allison gosta?',
+    'Instagram, Facebook e whatsapp.'
+)
